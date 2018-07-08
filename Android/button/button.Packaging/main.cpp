eb336580d27063ceba2d2c908c7fce9dbde2f42d
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2018-07-09 02:01:37 +0900

    Android版更新

--- Android/button/button.Packaging/main.cpp
+++ Android/button/button.Packaging/main.cpp
@@ -3,6 +3,8 @@
 
 //ペットの管理
 struct Pet {
+	
+	std::string name = u8"Jelly";
 	//UI
 	AnimeUI ui;
 	//体力
@@ -13,8 +15,8 @@ struct Pet {
 	float dis = 0.0f;
 
 	Pet() = default;
-	Pet(const AnimeMainData* const add_tmd, const uint8_t add_alpha, const Pos4& add_pos4, const float hp_) :ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
-	Pet(const AnimeMainData* const add_tmd, const uint8_t add_alpha, const PosA4F& add_pos4, const float hp_) : ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
+	const Pet(const AnimeMainData* const add_tmd, const uint8_t add_alpha, const Pos4& add_pos4, const float hp_) :ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
+	const Pet(const AnimeMainData* const add_tmd, const uint8_t add_alpha, const PosA4F& add_pos4, const float hp_) : ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
 	//const Pet& tmd(const AnimeMainData* const add_tmd) { ui. = add_tmd; }
 
 	Pet& move(std::vector<AnimeUI>& food)
@@ -35,42 +37,45 @@ struct Pet {
 		//餌を食べる
 		if (this->dis < ((ui.PosF().h > ui.PosF().w) ? ui.PosF().w / 6.0f : ui.PosF().h / 6.0f)) {
 			food.erase(food.begin() + dis_id);
-			if (this->hp > 100.0f) this->hp += 5.0f;
+			this->hp += float(asRand32(100)) / 10.0f;
+			if (this->hp > 100.0f) this->hp = 100.0f;
 		}
-
 		return *this;
 	}
 };
 
 int32_t asMain()
 {
-	//(174,221,222)
-	MainControl as(u8"Jellyfish", Pos2(1920, 1080), ColorRGB(0, 30, 67));
+	MainControl as(u8"Jellyfish", Pos2(1080, 1920), ColorRGB(0, 0, 0));
 	//画像ファイル
-	const AnimeMainData bgA(5, asLoadTex(u8"P/BG/bg.png", 1));
 	const AnimeMainData petA(32, asLoadTex(u8"P/Pet/pet.png", 8, 4));
 	const AnimeMainData foodA(1, asLoadTex(u8"P/Pet/food.png", 1));
-	const AnimeMainData twitterA(1, asLoadTex(u8"P/SNS/twitter.png", 1));
+	//const AnimeMainData twitterA(1, asLoadTex(u8"P/SNS/twitter.png", 1));
 
-	PosA4F p_a4(600.0f, 100.0f, 100.0f, 100.0f);
-	//asReadPos(u8"Save/pet0.txt", p_a4);
+	PosA4F p_a4(1000.0f, 300.0f, 100.0f, 100.0f);
+	PosA4F def_a4(p_a4);
+	float hp_def = 100.0f;
+
+	asReadPos(u8"Save/jelly_0_size_0.dat", p_a4);
+	asReadFloat(u8"Save/jelly_0_hp_0.dat", hp_def);
 
 	//ペット
-	Pet pet(&petA, 200, p_a4, 10.0f);
+	Pet pet(&petA, 200, p_a4, hp_def);
 	//餌
 	std::vector<AnimeUI> food;
-	//food.resize(size_t(asRand8(10)));
-	//for (AnimeUI& i : food) i.setUI(&foodA, 255, PosA4R(float(asRand32(asWindowSize().x)), 0.0f, 20.0f, 20.0f));
 
-	//SNS
-	AnimeUI twitterUI(&twitterA, 150, Pos4(100, 100, 200, 200));
-	Twitter t;
+	FontMainData font(asMakeFont(asWindowSize().y / 16));
 
 	//メインループ
 	while (as.loop())
 	{
-		bgA.draw(0, asWindowSize());
-		pet.move(food).ui.fpsUpdate().addSizeF(0.0001f, 400.0f).addRota(0.0f).drawRF();
+		font.drawAt(u8"Name: %s", Pos2(asWindowSize().x / 2, asWindowSize().y / 16), ColorRGBA(100, 100, 200, 255), pet.name.c_str());
+		font.drawAt(u8"Hp: %d", Pos2(asWindowSize().x / 2, asWindowSize().y * 2 / 16), ColorRGBA(100, 100, 200, 255), int32_t(pet.hp + 0.5f));
+		font.drawAt(u8"Size: %f", Pos2(asWindowSize().x / 2, asWindowSize().y * 3 / 16), ColorRGBA(100, 100, 200, 255), pet.ui.PosF().w);
+
+		pet.hp -= 0.000004f;
+		if (pet.hp < 0.0f) { pet.ui.setPosF(def_a4); pet.name = "Jelly"; pet.hp = 100.0f; }
+		pet.move(food).ui.fpsUpdate().addSizeF(0.000004f, 400.0f).addRota(0.0f).drawRF();
 		for (size_t i = 0; i < food.size(); ++i) {
 			//餌がウィンドウ外に出たら餌を消す
 			if (food[i].addPosF(0.0f, 0.3f).addRota(0.005f).drawRF().isOutWindowF()) { food.erase(food.begin() + i); --i; }
@@ -78,13 +83,12 @@ int32_t asMain()
 		//タッチされたら餌を出す
 		if (as.isUp()) {
 			food.emplace_back();
-			food[food.size() - 1].setUI(&foodA, 100, PosA4F(float(asRand32(asWindowSize().x)), 0.0f, 30.0f, 30.0f));
+			food[food.size() - 1].setUI(&foodA, 100, PosA4F(float(as.touchPos().x), 0.0f, 30.0f, 30.0f));
 		}
 
-		//if (twitterUI.draw().update().Up()) {
-		//	t.makeUrl(u8"https://goo.gl/DMh1QF").hashtags(u8"test").related(u8"wanotaitei").text(u8"%E3%82%AF%E3%83%A9%E3%82%B2%E3%82%92%E8%82%B2%E6%88%90%E4%B8%AD%E2%80%A6%E2%80%A6").send();
-		//}
 	}
-	asWritePos(u8"Save/pet0.txt", pet.ui.PosF());
+
+	asWritePos(u8"Save/jelly_0_size_0.dat", pet.ui.PosF());
+	asWriteFloat(u8"Save/jelly_0_hp_0.dat", pet.hp);
 	return 0;
 }
--- Windows/pet/Source.cpp
+++ Windows/pet/Source.cpp
@@ -3,6 +3,8 @@
 
 //ペットの管理
 struct Pet {
+
+	std::string name = u8"Jelly";
 	//UI
 	AnimeUI ui;
 	//体力
@@ -13,8 +15,8 @@ struct Pet {
 	float dis = 0.0f;
 
 	Pet() = default;
-	const Pet(const AnimeMainData* const add_tmd, const uint8_t add_alpha, const Pos4& add_pos4, const float hp_) :ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
-	const Pet(const AnimeMainData* const add_tmd, const uint8_t add_alpha, const PosA4F& add_pos4, const float hp_) : ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
+	Pet(const AnimeMainData* add_tmd, const uint8_t add_alpha, const Pos4& add_pos4, const float hp_) :ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
+	Pet(const AnimeMainData* add_tmd, const uint8_t add_alpha, const PosA4F& add_pos4, const float hp_) : ui(add_tmd, add_alpha, add_pos4), hp(hp_) {}
 	//const Pet& tmd(const AnimeMainData* const add_tmd) { ui. = add_tmd; }
 
 	Pet& move(std::vector<AnimeUI>& food)
@@ -35,7 +37,8 @@ struct Pet {
 		//餌を食べる
 		if (this->dis < ((ui.PosF().h > ui.PosF().w) ? ui.PosF().w / 6.0f : ui.PosF().h / 6.0f)) {
 			food.erase(food.begin() + dis_id);
-			if (this->hp > 100.0f) this->hp += 5.0f;
+			this->hp += float(asRand32(100)) / 10.0f;
+			if (this->hp > 100.0f) this->hp = 100.0f;
 		}
 		return *this;
 	}
@@ -43,24 +46,42 @@ struct Pet {
 
 int32_t asMain()
 {
-	MainControl as(u8"Jellyfish", Pos2(1920, 1080), ColorRGB(0, 0, 0));
+	MainControl as(u8"Jellyfish", Pos2(540,960), ColorRGB(0, 0, 0));
 	//画像ファイル
 	const AnimeMainData petA(32, asLoadTex(u8"P/Pet/pet.png", 8, 4));
 	const AnimeMainData foodA(1, asLoadTex(u8"P/Pet/food.png", 1));
-	const AnimeMainData twitterA(1, asLoadTex(u8"P/SNS/twitter.png", 1));
+	//const AnimeMainData twitterA(1, asLoadTex(u8"P/SNS/twitter.png", 1));
 
 	PosA4F p_a4(1000.0f, 300.0f, 100.0f, 100.0f);
+	PosA4F def_a4(p_a4);
+	float hp_def = 100.0f;
+	time_t time_def;
+
+	asReadPos(u8"jelly_0_size_0.dat", p_a4);
+	asReadFloat(u8"jelly_0_hp_0.dat", hp_def);
+	if (asReadTime(u8"time_0.dat", time_def) == -1) time_def = time(nullptr);
+	const float time_float = float((time(nullptr) - time_def));
+	hp_def -= time_float * 0.0002f;
+	p_a4.w += time_float * 0.000004f;
+	p_a4.h += time_float * 0.000004f;
 
 	//ペット
-	Pet pet(&petA, 200, p_a4, 10.0f);
+	Pet pet(&petA, 200, p_a4, hp_def);
 	//餌
 	std::vector<AnimeUI> food;
+
+	FontMainData font(asMakeFont(asWindowSize().x / 16));
 
 	//メインループ
 	while (as.loop())
 	{
-		//bgA.draw(0,asWindowSize());
-		pet.move(food).ui.fpsUpdate().addSizeF(0.0001f, 400.0f).addRota(0.0f).drawRF();
+		font.drawAt(u8"Name: %s", Pos2(asWindowSize().x / 2, asWindowSize().x / 16), ColorRGBA(100, 100, 200, 255), pet.name.c_str());
+		font.drawAt(u8"Hp: %d", Pos2(asWindowSize().x / 2, asWindowSize().x * 2 / 16), ColorRGBA(100, 100, 200, 255), int32_t(pet.hp + 0.5f));
+		font.drawAt(u8"Size: %f", Pos2(asWindowSize().x / 2, asWindowSize().x * 3 / 16), ColorRGBA(100, 100, 200, 255), pet.ui.PosF().w);
+
+		pet.hp -= 0.000004f;
+		if (pet.hp < 0.0f) { pet.ui.setPosF(def_a4); pet.name = "Jelly"; pet.hp = 100.0f; }
+		pet.move(food).ui.fpsUpdate().addSizeF(0.000004f, 2000.0f).addRota(0.0f).drawRF();
 		for (size_t i = 0; i < food.size(); ++i) {
 			//餌がウィンドウ外に出たら餌を消す
 			if (food[i].addPosF(0.0f, 0.3f).addRota(0.005f).drawRF().isOutWindowF()) { food.erase(food.begin() + i); --i; }
@@ -68,8 +89,12 @@ int32_t asMain()
 		//タッチされたら餌を出す
 		if (as.isUp()) {
 			food.emplace_back();
-			food[food.size() - 1].setUI(&foodA, 100, PosA4F(float(asRand32(asWindowSize().x)), 0.0f, 30.0f, 30.0f));
+			food[food.size() - 1].setUI(&foodA, 100, PosA4F(float(as.touchPos().x), 0.0f, 30.0f, 30.0f));
 		}
 	}
+
+	asWritePos(u8"jelly_0_size_0.dat", pet.ui.PosF());
+	asWriteFloat(u8"jelly_0_hp_0.dat", pet.hp);
+	asWriteTime(u8"time_0.dat", time(nullptr));
 	return 0;
 }
